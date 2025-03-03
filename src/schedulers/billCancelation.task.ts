4a0c93278c9fc1e7d7d8492e0047241f32ca610a
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-03-03 16:13:49 +0330

    bill cancelation

--- src/schedulers/billCancelation.task.ts
+++ src/schedulers/billCancelation.task.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Cron, CronExpression, SchedulerRegistry } from "@nestjs/schedule";
 import { Model } from "mongoose";
+import { BillDocument } from "src/models/Bills.schema";
 import { SessionDocument } from "src/models/Sessions.schema";
 
 @Injectable()
@@ -9,13 +10,13 @@ export class BillCancelationTask {
     constructor(
         // ...
         private schedulerRegistry: SchedulerRegistry,
-        @InjectModel("Session") private readonly SessionModel: Model<SessionDocument>,
+        @InjectModel("Bill") private readonly BillModel: Model<BillDocument>,
     ) {}
 
     @Cron(CronExpression.EVERY_5_MINUTES, { name: "billCancelation", timeZone: "Asia/Tehran" })
     async job(): Promise<string | void> {
         // change status of 'planChange' bills that are 'notPaid' or 'pendingPayment' and 15 minutes passed their createdAt
-        await this.SessionModel.updateMany(
+        await this.BillModel.updateMany(
             { type: "planChange", status: { $in: ["notPaid", "pendingPayment"] }, createdAt: { $lt: new Date(Date.now() - 60 * 15 * 1000) } },
             { status: "canceled" },
             { limit: 500 },
